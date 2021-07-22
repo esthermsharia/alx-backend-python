6b4772613b940361a1064b049087befd19b948aa
kimengu-david <davidmutune200@gmail.com>
2021-07-22 20:10:38 +0000

    Add task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Defines a type annotated functions that concatenates 2 strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenates 2 strings and returns the concatenated string"""
+    return str1 + str2
