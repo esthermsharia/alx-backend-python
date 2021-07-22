07c66a73a4695404b4052f9b6f54bdcb0fababb7
kimengu-david <davidmutune200@gmail.com>
2021-07-22 20:24:44 +0000

    Add task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Defines at type annotated function that returns the
+   float representation of a string
+"""
+
+
+def to_str(n: float) -> str:
+    """Returns the string representation of a float."""
+    return str(n)
