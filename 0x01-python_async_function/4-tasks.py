cdd4e177fc2f757c2b387fecefb88e67232163b4
kimengu-david <davidmutune200@gmail.com>
2021-07-26 22:06:56 +0000

    Add task 4

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module defines a function that returns the list of delays for an
+   async task/
+"""
+
+import asyncio
+from typing import List
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """ Function returns a list of wait times for an async task""".
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    return [await task for task in asyncio.as_completed(tasks)]
