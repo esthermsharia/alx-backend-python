4be0bed47da82e6592e7e45b65666abbb9e2077a
kimengu-david <davidmutune200@gmail.com>
2021-08-20 22:15:12 +0000

    Add task 2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,7 +2,7 @@
 """Parametize a unit test"""
 import unittest
 from parameterized import parameterized
-
+from unittest.mock import patch
 from utils import access_nested_map
 
 
@@ -26,3 +26,17 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError) as error:
             access_nested_map(nested_map, path)
         self.assertEqual(error.exception.args[0], path[-1])
+
+
+class TestGetJson(unittest.TestCase):
+    """ Tests that the function utils.get_json returns expected result """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    @patch('test_utils.get_json')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """ tests utils.get_json returns the expected result """
+        mock_get.return_value = test_payload
+        res = get_json(test_url)
+        self.assertEqual(res, test_payload)
